5eb26066d4417d7216531e0aae246b2b2e08aa66
SimranPPatil <sppatil2@illinois.edu>
2016-06-08 20:15:49 -0400

    First Commit

---
+++ Root Project/js/scrollify.js
@@ -0,0 +1,14 @@
+$.scrollify({
+		section : ".example-classname",
+		interstitialSection : "",
+		easing: "easeOutExpo",
+		scrollSpeed: 1100,
+		offset : 0,
+		scrollbars: true,
+		standardScrollElements: "",
+		setHeights: true,
+		before:function() {},
+		after:function() {},
+		afterResize:function() {},
+		afterRender:function() {}
+	});
